938163ce7bbde61fbd2745336ca5250b31a48ae7
daemon369 <daemon336699@gmail.com>
2022-01-17 15:11:01 +0800

    use android-version-catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
 rootProject.name = "android-storage"
 include(":demo")
 include(":storage")
+
+
+enableFeaturePreview("VERSION_CATALOGS")
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/releases/") }
+    }
+    versionCatalogs {
+        create("libs") {
+            from("io.github.daemon369:android-version-catalog:0.0.12")
+        }
+    }
+}
