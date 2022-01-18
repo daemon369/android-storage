9784fc515f67546ea0ac0455a7e142803b3f5829
daemon369 <daemon336699@gmail.com>
2022-01-18 10:33:49 +0800

    publish to maven

--- storage/build.gradle.kts
+++ storage/build.gradle.kts
@@ -3,6 +3,27 @@ plugins {
     id("kotlin-android")
 }
 
+ext {
+    set("artifactId", "android-storage")
+    set("artifactVersion", "0.0.1")
+
+    set(
+        "pom",
+        mapOf(
+            "name" to get("artifactId"),
+            "description" to "Android Storage",
+            "url" to "https://github.com/daemon369/android-storage",
+            "scm" to mapOf(
+                "connection" to "scm:git:git://github.com/daemon369/android-storage.git",
+                "developerConnection" to "scm:git:ssh://github.com/daemon369/android-storage.git",
+                "url" to "https://github.com/daemon369/android-storage/tree/main",
+            )
+        )
+    )
+}
+
+val artifactId: String by extra
+
 android {
     compileSdk = 31
 
@@ -40,3 +61,5 @@ dependencies {
     androidTestImplementation(libs.x.junit)
     androidTestImplementation(libs.x.espresso.core)
 }
+
+apply(from = "$rootDir/gradle/maven-publish.gradle")
